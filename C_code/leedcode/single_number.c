3cf6b91c96b583553b0a5d57f31b1b1a7efd58bf
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2020-06-09 00:52:27 +0800

    Update single_number.c

--- C_code/leedcode/single_number.c
+++ C_code/leedcode/single_number.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-/*
+/*Single Number
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 Note:
