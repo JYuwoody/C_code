29619ac24e1d9ac1556672171c5be748be30a37a
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2020-07-22 01:42:02 +0800

    Update and rename Best_Time_to_Buy_and_Sell_Stock_II.c to 00122_Best_Time_to_Buy_and_Sell_Stock_II.c

--- C_code/leedcode/00122_Best_Time_to_Buy_and_Sell_Stock_II.c
+++ C_code/leedcode/00122_Best_Time_to_Buy_and_Sell_Stock_II.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-/* 112 Best Time to Buy and Sell Stock II
+/* 122 Best Time to Buy and Sell Stock II
 Say you have an array prices for which the ith element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
