d3743c9d726bb91c23c693ed2c660b8c2a6e9359
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2020-08-06 15:16:08 +0800

    Update 00000_leetcode.c

--- C_code/leedcode/00000_leetcode.c
+++ C_code/leedcode/00000_leetcode.c
@@ -30,11 +30,29 @@ Given a m x n grid filled with non-negative numbers, find a path from top left t
 */
 int minPathSum(int** grid, int gridSize, int* gridColSize)
 {
-    //11110
-    //01010
-    //10001
-    //11110
-    int NUMBER_OF_ROWS = gridSize;  //4
-    int NUMBER_OF_COLS = gridColSize[0];  //5
 
 }
+
+/*
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        if (m == 0) return 0;
+        int n = grid[0].size();
+        
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) 
+                    grid[i][j] += grid[i][j - 1];
+                else if (j == 0)
+                    grid[i][j] += grid[i - 1][j];
+                else
+                    grid[i][j] += min(grid[i][j - 1], grid[i - 1][j]);
+            }
+        
+        return grid[m - 1][n - 1];
+    }    
+};
+*/
