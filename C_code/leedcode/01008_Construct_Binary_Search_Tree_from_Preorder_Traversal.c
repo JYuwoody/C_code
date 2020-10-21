2ebb7261f989327f9ef71b0c186b1a7835a3459f
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2020-10-22 00:33:16 +0800

    Update 01008_Construct_Binary_Search_Tree_from_Preorder_Traversal.c

--- C_code/leedcode/01008_Construct_Binary_Search_Tree_from_Preorder_Traversal.c
+++ C_code/leedcode/01008_Construct_Binary_Search_Tree_from_Preorder_Traversal.c
@@ -7,20 +7,13 @@
 //1008. Construct Binary Search Tree from Preorder Traversal
 /*
 Return the root node of a binary search tree that matches the given preorder traversal.
-
 (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
-
 It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
-
 Example 1:
-
 Input: [8,5,1,7,10,12]
 Output: [8,5,10,1,7,null,12]
-
  
-
 Constraints:
-
 1 <= preorder.length <= 100
 1 <= preorder[i] <= 10^8
 The values of preorder are distinct.
@@ -36,6 +29,7 @@ struct TreeNode {
 
 struct TreeNode* bstFromPreorder(int* preorder, int preorderSize);
 struct TreeNode* bstFromPreorderRange(int* preorder, int start, int end);
+struct TreeNode* bstFromPreorder_forloop(int* preorder, int preorderSize);
 
 void main()
 {
@@ -76,4 +70,54 @@ struct TreeNode* bstFromPreorder(int* preorder, int preorderSize)
     // preorder [8,5,1,7,10,12] preorderSize = 6
 
     return bstFromPreorderRange(preorder, 0, preorderSize);
+}
+
+
+struct TreeNode* bstFromPreorder_forloop(int* preorder, int preorderSize)
+{
+    // preorder [8,5,1,7,10,12] preorderSize = 6
+    int i = 0;
+    struct TreeNode* path[100];
+    struct TreeNode* root = malloc(sizeof(struct TreeNode));
+    int topIndex = 0;
+
+    root->val = preorder[0];
+    root->left = root->right = NULL;
+
+    path[topIndex] = root;
+
+    for(i=1;i<preorderSize;i++)
+    {
+        struct TreeNode* node = malloc(sizeof(struct TreeNode));
+        node->val = preorder[i];
+        node->left = node->right = NULL;
+
+        if(preorder[i] < path[topIndex]->val)
+        {
+            path[topIndex]->left = node;
+            topIndex++;
+            path[topIndex] = node;
+        }
+        else
+        {
+            while( (topIndex-1 >= 0) && (path[topIndex-1]->val < preorder[i]) )
+            {
+                topIndex--;
+            }
+            if(topIndex-1 >= 0)
+            {
+                path[topIndex]->right = node;
+                path[topIndex] = node;
+            }
+            else
+            {
+                path[0]->right = node;
+                path[0] = node;
+            }
+            
+        }
+        
+        //free(node);
+    }
+    return root;
 }
