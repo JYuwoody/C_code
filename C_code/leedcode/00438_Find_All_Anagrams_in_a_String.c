ddfe5ffdd33a6f2829f963abbaae27ab2d8e4f36
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2021-12-14 14:22:55 +0800

    Update 00438_Find_All_Anagrams_in_a_String.c

--- C_code/leedcode/00438_Find_All_Anagrams_in_a_String.c
+++ C_code/leedcode/00438_Find_All_Anagrams_in_a_String.c
@@ -106,7 +106,43 @@ int* findAnagrams2(char * s, char * p, int* returnSize)
     *returnSize = resultLen;
     return result;
 }
+/* https://hackmd.io/@kenjin/BkO3caEGH
+ * Runtime: 28 ms, faster than 92.68% of C online submissions for Find All Anagrams in a String.
+ * Memory Usage: 9.5 MB, less than 67.07% of C online submissions for Find All Anagrams in a String.
+*/
+int* findAnagrams_b(char * s, char * p, int* returnSize)
+{
+    int alpha[26] = {0};
+    int alphaCal[26] = {0};
+    int pLen = strlen(p);
+    int sLen = strlen(s);        
+    int *ret = malloc(sizeof(int)*sLen);
+    int retCtr = 0;
+    *returnSize = 0;
 
+    for (int i = 0; i < pLen; i++)
+    {
+        alpha[(p[i] - 'a')] += 1;
+    }
+    for (int i = 0; i < sLen; i++)
+    {
+        int curIdx = s[i] - 'a';
+        if (i >= pLen)
+        {
+            int startIdx = s[i-pLen] - 'a';
+            alphaCal[startIdx]--;
+        }
+        alphaCal[curIdx]++;
+        if (!memcmp(alphaCal, alpha, 26*sizeof(int)))
+        {
+            ret[retCtr] = i - pLen + 1;
+            retCtr++;
+        }
+    }
+    
+    *returnSize = retCtr;
+    return ret;
+}
 
 
 /**
