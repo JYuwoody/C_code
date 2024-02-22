9c3eb6956f341621b62c9c0c472aa9653e768862
JYuwoody <30628226+JYuwoody@users.noreply.github.com>
2024-02-22 11:53:57 +0800

    00082

--- C_code/leedcode/00000_leetcode.c
+++ C_code/leedcode/00000_leetcode.c
@@ -4,61 +4,53 @@
 #include<math.h>
 #include<stdbool.h>
 #include<string.h>
-
-/*00986. Interval List Intersections
-Medium
-
-You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
-
-Return the intersection of these two interval lists.
-
-A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
-
-The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+ 
+/*82. Remove Duplicates from Sorted List II
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
 
  
 
 Example 1:
 
 
-Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
-Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
 Example 2:
 
-Input: firstList = [[1,3],[5,9]], secondList = []
-Output: []
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
  
 
 Constraints:
 
-0 <= firstList.length, secondList.length <= 1000
-firstList.length + secondList.length >= 1
-0 <= starti < endi <= 109
-endi < starti+1
-0 <= startj < endj <= 109
-endj < startj+1
-*/
-
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
  */
-int** intervalIntersection(int** firstList, int firstListSize, int* firstListColSize, 
-                           int** secondList, int secondListSize, int* secondListColSize, 
-                           int* returnSize, int** returnColumnSizes)
-{
-    //firstList = [[0,2],[5,10],[13,23],[24,25]], firstListSize=4, firstListColSize=[2,2,2,2]
-    //secondList = [[1,5],[8,12],[15,24],[25,26]], secondListSize=4, secondListColSize[2,2,2,2]
-    //ans = [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]], *returnSize = 6, *returnColumnSizes = ansColSize // [2,2,2,2,2,2]
-    int** ans = NULL;
-    int ansSize = 0;
 
-    *returnSize = ansSize;
-    int *ansColSize = malloc(ansSize*sizeof*(int));
-    *returnColumnSizes = ansColSize;
-    return ans;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+// Function to create a new node
+struct ListNode* newNode(int val) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
 }
 
 
-//https://www.youtube.com/watch?v=fT49oFlbgW4&list=PLY_qIufNHc292EYDwe3B6sGYO_vQ7pOLx&index=23
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (head == NULL) return NULL;
+    
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode* prev = &dummy;
+    struct ListNode* curr = head;    
+
+    
+}
